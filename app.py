f62379e2ccfa2ec2d541cce4c27cc88e8179f801
bsoni <sonibhavinb@gmail.com>
2019-08-09 12:05:07 -0400

    frontend

--- app.py
+++ app.py
@@ -35,11 +35,11 @@ app = Flask(__name__)
 database.connect()
 #Result form class
 class Song:
-    def __init__(self, id, name, artist, link, img):
+    def __init__(self, id, name, artist, img):
         self.id = id
         self.name  = name
         self.artist = artist
-        self.link = link
+        #self.link = link
         self.img = img
 
 class Note:
@@ -51,9 +51,46 @@ class Note:
 
 #Mock dataset of songs
 songs = []
-for i in range(9):
-    song = Song(i, "song_{}".format(i), "Taylor Swift","https://soundcloud.com/taylorswiftofficial/i-did-something-bad", 'https://i1.sndcdn.com/artworks-NMKb89tCPSQP-0-t500x500.jpg')
-    songs.append(song)
+#for i in range(15):
+#    song = Song(i, "song_{}".format(i), "Taylor Swift","https://soundcloud.com/taylorswiftofficial/i-did-something-bad", 'https://i1.sndcdn.com/artworks-NMKb89tCPSQP-0-t500x500.jpg')
+#    songs.append(song)
+
+
+song0 = Song(0, "Ran$om", "Lil Tecca", 'https://upload.wikimedia.org/wikipedia/en/8/86/Lil_Tecca_-_Ransom.png')
+songs.append(song0)
+
+song1 = Song(1, "Boyfriend", "Ariana Grande", 'http://cdn02.cdn.justjared.com/wp-content/uploads/headlines/2019/08/ariana-grande-boyfriend-stream.jpg')
+songs.append(song1)
+
+song2 = Song(2, "Goodbyes", "Post Malone", 'https://pl.scdn.co/images/pl/default/8ad229b50c9ab13c0c233dc987e8b59a6e1746c5')
+songs.append(song2)
+
+song3 = Song(3, "Truth Hurts", "Lizzo", 'https://s3.amazonaws.com/media.thecrimson.com/photos/2019/05/04/135013_1338000.png')
+songs.append(song3)
+
+song4 = Song(4, "Money in the Grave", "Drake", 'https://pl.scdn.co/images/pl/default/1148f1bad87bb7dfbe8e8d8374a66fd8eac6ed71')
+songs.append(song4)
+
+song5 = Song(5, "I Don't Care", "Ed Sheeran",'https://upload.wikimedia.org/wikipedia/en/6/69/Ed_Sheeran_%26_Justin_Bieber_-_I_Don%27t_Care.png')
+songs.append(song5)
+
+song6 = Song(6, "Old Town Road", "Lil Nas X",'https://img.discogs.com/zOLHNMG4CR9fgLs4wfJd7T566LA=/fit-in/300x300/filters:strip_icc():format(jpeg):mode_rgb():quality(40)/discogs-images/R-13475626-1554921221-3492.jpeg.jpg')
+songs.append(song6)
+
+song7 = Song(7, "Bad Guy", "Billie Eilish", 'http://d2lubch9d26anb.cloudfront.net/cdn/farfuture/QnhjlVPapAC_e9SVZ242t0OCLM_CCC4Stnx5FQsT2P0/mtime:1548794286/sites/default/files/styles/album_artwork__300x300_/public/BAA434A0-9DBB-4216-B605-0510C18C54CA.jpeg?itok=VT9atKhn')
+songs.append(song7)
+
+song8 = Song(8, "God's Country", "Blake Shelton", 'https://upload.wikimedia.org/wikipedia/en/b/b2/Blake_Shelton_-_God%27s_Country.png')
+songs.append(song8)
+
+song9 = Song(9, "High Hopes", "Panic! At The Disco", 'https://res.cloudinary.com/dx5kjjpce/image/upload/d_default.svg/2788f359d686032eeebbbb4c908ea9f6.jpg')
+songs.append(song9)
+
+song10 = Song(10, "Happier", "Marshmello", 'https://images.genius.com/4ff9277eed36fb6f90372654b6c9d818.300x300x1.png')
+songs.append(song10)
+
+song11 = Song(11, "Happier", "Marshmello", 'https://images.genius.com/4ff9277eed36fb6f90372654b6c9d818.300x300x1.png')
+songs.append(song11)
 
 
 note_lst = ['CITRUS',
@@ -132,6 +169,9 @@ def select_song():
             if str(song.id) in request.args:
                 result += [str(song.id)]
         url = "/notes/{}/{}/{}".format(result[0],result[1],result[2])
+        print(result[0])
+        print(result[1])
+        print(result[2])
         return redirect(url)       
 
       
